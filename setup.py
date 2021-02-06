ee28679694a9ae6d9442e74d92f46bec7481db21
Maksim Koltsov <tirinox@gmail.com>
2021-02-06 10:18:09 +0300

    fix setup.py

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ except ImportError:
 
 def load_requirements(fname):
     reqs = parse_requirements(fname, session="test")
-    return [str(ir.req) for ir in reqs]
+    return [str(ir.requirement) for ir in reqs]
 
 
 setuptools.setup(
