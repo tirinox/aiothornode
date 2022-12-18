c3f0fd7cd042b3ee0941256ee080a333a2d6374a
Tirinox <developer@tirinox.ru>
2022-12-18 17:16:53 +0300

    ThorNodeClient's joined into a single one

--- aiothornode/nodeclient.py
+++ aiothornode/nodeclient.py
@@ -1,37 +1,24 @@
 import logging
 
 import ujson
-from aiohttp import ClientSession
+from aiohttp import ClientSession, ClientTimeout
+from aiohttp.helpers import sentinel
 
 from aiothornode.env import ThorEnvironment
-
-THORNODE_PORT = 1317
-TENDERMINT_RPC_PORT_TESTNET = 26657
-TENDERMINT_RPC_PORT_MAINNET = 27147
 
 
 class ThorNodeClient:
     HEADER_CLIENT_ID = 'x-client-id'
 
-    def connection_url(self, ip_address, path, is_rpc):
-        port = self.tendermint_rpc_port if is_rpc else self.thornode_port
-        path = '/' + path.lstrip('/')
-        return f'http://{ip_address}:{port}{path}'
-
-    def __init__(self, node_ip: str, session: ClientSession, timeout=3.0, logger=None,
-                 thornode_port=THORNODE_PORT,
-                 tendermint_rpc_port=TENDERMINT_RPC_PORT_MAINNET,
-                 extra_headers=None):
+    def __init__(self, session: ClientSession, env: ThorEnvironment, logger=None, extra_headers=None):
         self.session = session
-        self.timeout = timeout
-        self.node_ip = node_ip
+        self.timeout = ClientTimeout(total=env.timeout) if env.timeout else sentinel
         self.logger = logger or logging.getLogger(self.__class__.__name__)
-        self.tendermint_rpc_port = tendermint_rpc_port
-        self.thornode_port = thornode_port
         self.extra_headers = extra_headers
+        self.env = env
 
     async def request(self, path, is_rpc=False):
-        url = self.connection_url(self.node_ip, path, is_rpc)
+        url = self.connection_url(path, is_rpc)
         self.logger.debug(f'Node GET "{url}"')
         async with self.session.get(url, timeout=self.timeout, headers=self.extra_headers) as resp:
             self.logger.debug(f'Node RESPONSE "{url}" code={resp.status}')
@@ -50,21 +37,10 @@ class ThorNodeClient:
             self.extra_headers[self.HEADER_CLIENT_ID] = client_id
 
     def __repr__(self) -> str:
-        return f'ThorNodeClient({self.node_ip!r})'
+        return f'ThorNodeClient({self.env.thornode_url!r})'
 
-
-# todo: join ^ and v
-class ThorNodePublicClient(ThorNodeClient):
-    def __init__(self, session: ClientSession, env: ThorEnvironment, logger=None, extra_headers=None):
-        port = TENDERMINT_RPC_PORT_TESTNET if env.kind == 'testnet' else TENDERMINT_RPC_PORT_MAINNET
-        super().__init__('', session, env.timeout, logger, 0, port, extra_headers=extra_headers)
-        self.env = env
-
-    def connection_url(self, ip_address, path, is_rpc):
+    def connection_url(self, path, is_rpc):
         if is_rpc:
             return f'{self.env.rpc_url}{path}'
         else:
             return f'{self.env.thornode_url}{path}'
-
-    def __repr__(self) -> str:
-        return f"ThorNodePublicClient({self.env.thornode_url})"
