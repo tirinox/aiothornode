a7cb4bc5772542f4c52da37becccd70b86807bbc
Tirinox <developer@tirinox.ru>
2022-12-24 21:54:27 +0300

    Bump v.0.1.5

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as f:
 
 setuptools.setup(
     name='aiothornode',
-    version='0.1.4',
+    version='0.1.5',
     description='THORChain node connection library for Python',
     long_description=long_description,
     long_description_content_type='text/markdown',
